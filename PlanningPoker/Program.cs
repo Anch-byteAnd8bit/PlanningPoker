5bc6915efd6fc65b0675df58f766dfdcd0a4f1ac
PNZeml <pavelz1n@yandex.ru>
2023-12-28 07:25:06 +0000

    feat: add telegram http client

--- PlanningPoker/Program.cs
+++ PlanningPoker/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using PlanningPoker;
+using Telegram.Bot;
 
 var hostBuilder = Host.CreateEmptyApplicationBuilder(new() { Args = args });
 
@@ -29,5 +31,12 @@ public static class ServiceCollectionExtensions {
     public static void AddTelegramBotService(this IServiceCollection services) {
         services.AddOptions<TelegramBotOptions>()
             .BindConfiguration(TelegramBotOptions.ConfigurationSectionPath);
+
+        services.AddHttpClient("telegram-bot-client")
+            .AddTypedClient<ITelegramBotClient>(static (httpClient, services) => {
+                var options = services.GetRequiredService<IOptions<TelegramBotOptions>>().Value;
+
+                return new TelegramBotClient(options: new(options.Token), httpClient);
+            });
     }
 }
