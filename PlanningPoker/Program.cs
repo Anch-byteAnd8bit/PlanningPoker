39985888ba33e0c735df20f241943daef24a4284
PNZeml <pavelz1n@yandex.ru>
2023-12-28 06:45:54 +0000

    refact: add typed telegram bot configuration

--- PlanningPoker/Program.cs
+++ PlanningPoker/Program.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using PlanningPoker;
 
 var hostBuilder = Host.CreateEmptyApplicationBuilder(new() { Args = args });
+
+hostBuilder.Configuration.AddJsonFile("appsettings.json", optional: false);
+
+hostBuilder.Services.AddTelegramBotService();
 
 var host = hostBuilder.Build();
 
@@ -12,3 +18,16 @@ if (hostBuilder.Configuration["Bot:Token"] is { } token)
     host.Run();
 }
 else Console.WriteLine("Не удалось получить Token");
+
+public sealed class TelegramBotOptions {
+    public const string ConfigurationSectionPath = "Services:TelegramBot";
+
+    public required string Token { get; init; }
+}
+
+public static class ServiceCollectionExtensions {
+    public static void AddTelegramBotService(this IServiceCollection services) {
+        services.AddOptions<TelegramBotOptions>()
+            .BindConfiguration(TelegramBotOptions.ConfigurationSectionPath);
+    }
+}
