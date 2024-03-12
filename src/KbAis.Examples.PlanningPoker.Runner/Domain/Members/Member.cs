09cfaa15a7f8041f043c942e45b4b7a572892191
Anna <ragebluhane@gmail.com>
2024-03-13 00:22:54 +0700

    new project registration and notification

--- src/KbAis.Examples.PlanningPoker.Runner/Domain/Members/Member.cs
+++ src/KbAis.Examples.PlanningPoker.Runner/Domain/Members/Member.cs
@@ -3,5 +3,6 @@ using CSharpFunctionalExtensions;
 namespace KbAis.Examples.PlanningPoker.Runner.Domain.Members;
 
 public class Member : Entity {
+    public long UserId { get; init; }
     public ChatInfo Chat { get; init; }
 }
